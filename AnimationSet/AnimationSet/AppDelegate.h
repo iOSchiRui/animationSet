6161e30a71f0f2baebe828098489b6098baf11b6
chirui <474479892@qq.com>
2019-09-24 17:46:18 +0800

    create new project

---
+++ AnimationSet/AnimationSet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationSet
+//
+//  Created by Seven on 2019/9/24.
+//  Copyright © 2019 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationSet/AnimationSet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationSet
+//
+//  Created by Seven on 2019/9/24.
+//  Copyright © 2019 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
